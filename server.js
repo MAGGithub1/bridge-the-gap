b7fab8116fdd09dca6ee417a7e9622a57d023eff
Meredith Gibbons <mgibbons1705@yahoo.co.uk>
2020-03-25 02:18:34 +0000

    Started lobby implementation

--- server.js
+++ server.js
@@ -12,23 +12,60 @@ let server = http.Server(app);
 let io = socketIO(server);
 app.set('port', 5000);
 
-
+// get files
 app.use('/static', express.static(__dirname + '/static'));
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+app.get('/index.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.css'));
+});
+app.get('/lobby', function (req, res) {
+  res.sendFile(path.join(__dirname, 'lobby.html'));
 });
 
 // Starts the server.
-server.listen(5000, function() {
+server.listen(5000, function () {
   console.log('Starting server on port 5000');
 });
 
-// Add the WebSocket handlers
-io.on('connection', function(socket) {
+// lobby names are defined by the users
+
+lobbies_open = {};    // lobbies that can currently be joined
+lobbies_closed = {};  // lobbies that cannot be joined
+
+// handle socket.io connections
+io.on('connection', function (socket) {
   // for testing
-  socket.on('keydown req', function(data){
+  socket.on('keydown req', function (data) {
     res = 'player ' + socket.id + ' pressed key ' + data;
     io.sockets.emit('keydown res', res);
+  });
+
+  // creation of a new lobby
+  socket.on('new lobby', function (name) {
+    // todo check the lobby with name = name does not exist
+
+    // if it does exist, add lobby and player
+
+    lobbies_open[name] = [socket.id];
+
+    // send the updated list of available lobbies to everyone
+    io.sockets.emit('new lobby available', Object.keys(lobbies_open));
+
+    // let the client know they have joined the lobby
+    socket.broadcast.to(socket.id).emit('joined lobby', { lobby_name: name, admin: true });
+  });
+
+  socket.on('join lobby', function (name) {
+    // todo check the lobby with name = name exists
+
+    // if it does exist, add lobby and player
+    lobbies_open[name].push(socket.id);
+
+    // let the client know they have joined the lobby
+    socket.broadcast.to(socket.id).emit('joined lobby', { lobby_name: name, admin: false });
+
   });
 });
 
--- static/game.js
+++ static/game.js
@@ -1,8 +1,21 @@
 let socket = io();
-socket.on('keydown res', function(data){
-  console.log(data);
+
+socket.on('new lobby available', function(data){
+  console.log("lobbies available: ", data)
+})
+socket.on('joined lobby', function(data){
+  console.log("joined lobby ", data);
 });
 
-document.addEventListener('keydown', function(event){
-  socket.emit('keydown req', event.key);
-});
+
+// entry page
+
+function createLobby(lobby_name) {
+  console.log("creating lobby");
+  socket.emit('new lobby', lobby_name);
+}
+
+function joinLobby(lobby_name) {
+  console.log("joining lobby");
+  socket.emit('join lobby', lobby_name)
+}
